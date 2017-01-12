c566a472c34ca2c4ece5e64cd0963ba7e83f695a
rosstapson <ross.tapson@gmail.com>
2017-01-12 13:42:46 +0200

    motto

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/seekerDNA',
   port: process.env.PORT || 3001,
-  secret: "I drink and code things."
+  secret: **my tyrion motto**
   
 };
 module.exports = config;
