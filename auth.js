73ffe8ee96482aed7bf1287deaecc5906c237186
rosstapson <ross.tapson@gmail.com>
2017-02-03 10:12:06 +0200

    zomg.

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 export function checkToken(req) {
-  var token = req.body.id_token || req.query.id_token;
+  var token = req.body.id_token || req.query.id_token || req.headers['x-access-token'];
   console.log(token);
   return true;
 }
