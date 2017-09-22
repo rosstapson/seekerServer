40b522d9da904e69b515122dd352e3afdf8dc18a
Ross Tapson <ross.tapson@gmail.com>
2017-09-22 09:38:27 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/seekerDNA',
-  port: process.env.PORT || 3001,
-  secret: **my tyrion motto**
-  
-};
-module.exports = config;
